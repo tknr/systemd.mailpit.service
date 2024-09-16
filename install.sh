53ac6fec3b41b1404fb2a2e67556ab80435b4353
tknr <geishagirlskoume@gmail.com>
2024-09-16 14:49:14 +0900

    u

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 cd `dirname $0`
 bash < <(curl -sL https://raw.githubusercontent.com/axllent/mailpit/develop/install.sh)
-cp -f mailhog.service /etc/systemd/system/
+/bin/cp -vf mailpit.service /etc/systemd/system/
